1265901c4dc9310f4ee39a0e05fd4238582381a3
Xaxetrov <edern.haumont@orange.fr>
2015-10-26 12:17:29 +0100

    bresenham coming soon !

--- shootablerange.cpp
+++ shootablerange.cpp
@@ -3,21 +3,21 @@
 ShootableRange::ShootableRange(const Map &map,const Coordinate centerPos, int type =0, int maxRange =0, int minRange =0) :
     m_absolutePosOnMap(centerPos), Range(type, maxRange, minRange)
 {
-    m_shootableTiles = new vector<vector<byte>>;
-    for(byte i=0; i<2*maxRange+1; i++)
+    m_shootableTiles = new vector<vector<unsigned char>>;
+    for(unsigned char i=0; i<2*maxRange+1; i++)
     {
-        vector<byte> column;
+        vector<unsigned char> column;
         m_shootableTiles.push_back(column);
         for(byte j=0; j<2*radius+1; j++)
         {
             m_shootableTiles.at(i).push_back(0);
         }
     }
-    byte m_center = m_shootableTiles.size()/2;
+    unsigned char m_center = m_shootableTiles.size()/2;
     Coordinate upLeftCrd(m_absolutePosOnMap-maxRange, m_absolutePosOnMap-maxRange);
-    for (byte radius=minRange; radius<=maxRange; ++radius)
+    for (unsigned char radius=minRange; radius<=maxRange; ++radius)
     {
-        for (byte i=m_center; i<=m_center+radius; i++)
+        for (unsigned char i=m_center; i<=m_center+radius; i++)
         {
             computeTile(map, Coordinate(2*m_center-i,-radius+1));
             computeTile(map, Coordinate(2*m_center-i,2*m_center+radius-i));
@@ -40,16 +40,41 @@ void ShootableRange::computeTile(const Map &map, const Coordinate crd, const Coo
     m_shootableTiles.at(crd.getX()).at(crd.getY()) = bresenham(crd)
 }
 
-byte ShootableRange::bresenham(const Coordinate crd)
+unsigned char ShootableRange::bresenham(const Coordinate crd)
 {
     Coordinate octantZeroCrd;
+    if(crd.getX()==m_center)
+    {
+
+    }
+    else if(crd.getY()==m_center)
+    {
+
+    }
+    else
+    {
+        unsigned char octant;
+        dYdX = (crd.getY()-m_center)/(crd.getX()-m_center);
+        if(dYdX >0)
+        {
+
+        }
+    }
 }
 
 Coordinate ShootableRange::switchToOctantZeroFrom(byte octant, Coordinate crd)
 {
     switch(octant)
     {
-
+    case 0:
+        return Coordinate;
+    case 1 :
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+    case 6 :
+    case 7 :
     }
 }
 
@@ -57,7 +82,15 @@ Coordinate ShootableRange::switchFromOctantZeroTo(octant, Coordinate crd)
 {
     switch(octant)
     {
-
+    case 0:
+        ;
+    case 1 :
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+    case 6 :
+    case 7 :
     }
 }
 
--- shootablerange.h
+++ shootablerange.h
@@ -7,17 +7,17 @@ class ShootableRange : public range
 {
 public:
     ShootableRange(const Map &map,const Coordinate centerPos, int type =0, int maxRange =0, int minRange =0);
-    std::vector<std::vector<byte>> getShootableTiles();
+    std::vector<std::vector<unsigned char>> getShootableTiles();
 
 private :
     void computeTile(const Map &map, const Coordinate crd);
-    byte bresenham(const Coordinate crd);
-    Coordinate switchToOctantZeroFrom(byte octant, Coordinate crd);
-    Coordinate switchFromOctantZeroTo(byte octant, Coordinate crd);
+    unsigned char bresenham(const Coordinate crd);
+    Coordinate switchToOctantZeroFrom(unsigned char octant, Coordinate crd);
+    Coordinate switchFromOctantZeroTo(unsigned char octant, Coordinate crd);
 
-    std::vector<std::vector<byte>> m_shootableTiles;
+    std::vector<std::vector<unsigned char>> m_shootableTiles;
     Coordinate m_absolutePosOnMap;
-    byte m_center;
+    unsigned char m_center;
 };
 
 #endif // SHOOTABLERANGE_H
